bf81b242f48affec7f99698d5c6b5dda7a082484
David Munyiri <davidmumyiri2019@gmail.com>
2023-01-19 08:21:19 -0800

    made a single change

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 "use strict";
 const button = document.querySelector(".btn");
-function buy() {
-  prompt("DO you want to buy the Sneaker");
-}
+
 button.addEventListener("click", function () {
-  buy();
-  if (buy() === "yes") {
+  const buy = prompt("DO you want to buy the Sneaker");
+  if (buy.valueOf === "yes") {
     button.textContent = "BOUGHT";
   }
 });
