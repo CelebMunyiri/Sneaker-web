056304b9d50a099453afbbdb68d4535324b8d37d
David Munyiri <davidmumyiri2019@gmail.com>
2023-01-22 09:28:58 -0800

    added a functionality

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 "use strict";
 const button = document.querySelector(".btn");
+let cart = {};
+
+//A function to add items tothe cart
+function addCart(item, quantity) {}
 
 button.addEventListener("click", function () {
   const buy = prompt("DO you want to buy the Sneaker");
+
   if (buy === "yes") {
     button.textContent = "BOUGHT";
   }
